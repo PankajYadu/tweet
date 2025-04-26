d3ad2b6060461b142e89d011bd44efb6e47dc4b8
PankajYadu <techypan@gmail.com>
2025-04-26 20:13:28 +0530

    Add gunicorn and whitenoise for deployment

--- twitter_scrap/settings.py
+++ twitter_scrap/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',                #only added for deployment on render.com
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
